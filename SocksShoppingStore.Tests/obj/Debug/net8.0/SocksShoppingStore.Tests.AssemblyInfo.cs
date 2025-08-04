6aa75b2d9df4b78ae497bc452ffb87b7e292958f
rudExtremo <118679491+rudExtremo@users.noreply.github.com>
2025-08-04 12:31:51 +0100

    Fix  yaml

--- SocksShoppingStore.Tests/obj/Debug/net8.0/SocksShoppingStore.Tests.AssemblyInfo.cs
+++ SocksShoppingStore.Tests/obj/Debug/net8.0/SocksShoppingStore.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocksShoppingStore.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ef4899a70f0a0bd33e1569c77adf6f573d21467")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d82d134be41e62e000648e2df67b6809284f02ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocksShoppingStore.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocksShoppingStore.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
